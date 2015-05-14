a0389b383201fa34304e94fa54e02a2c4a91eff1
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-05-14 19:36:20 +0200

    test 5 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -3,9 +3,15 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
-		for(Vm vm : vms) {
-			servers[0].addVm(vm);
+		for (Vm vm : vms) {
+			Server lessLoaded = null;
+			for (Server server : servers) {
+				if (lessLoaded == null
+						|| lessLoaded.currentLoadPecentage > server.currentLoadPecentage) {
+					lessLoaded = server;
+				}
+			}
+			lessLoaded.addVm(vm);
 		}
-
 	}
 }
